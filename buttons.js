e56948e13dc1916b225b31309930148bffaccd32
sudhas121 <sudhas121@hotmail.com>
2023-05-17 20:03:57 -0700

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,218 @@
+    <!DOCTYPE html>
+    <html>
+    <title>Divine Traveller</title>
+    <meta charset="UTF-8">
+    
+    <META NAME="description" CONTENT=" temples in India,108 divya desams,South Indian temples,Ganesha temples"/>
+    <META NAME="keywords" CONTENT="Mumbai,Sidhi Vinayak,Prabhadevi, Sree Sidhi Vinayak Maharaj, Ganesha,Madurai meenakshi, Kanchipuram,Chennai Temples, Mylapore,Kapaleeswarar, 108 divya desams"/>
+        
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet"  type="text/css" href="w3-css.css"> 
+    <link rel="stylesheet"  type="text/css" href="divtr-css.css"> 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+
+       <script language="JavaScript" type="text/javascript">
+
+        if (document.getElementById) 
+        {
+         document.writeln('<style type="text/css"> ')
+         document.writeln('.main {text-decoration:none; color:black; cursor:hand; cursor:pointer;margin-bottom: 1px; background-color:#6699ff; float:centre   }')
+         document.writeln('span:hover.mainLink {text-decoration:none; color:red}')
+            document.writeln('.sub {text-decoration:none; color:black; cursor:hand; cursor:pointer;margin-bottom: 1px; background-color:#0000FF; float:centre   }')
+         document.writeln('.links {display:none; padding-left:1px}')
+         document.writeln('.link {text-decoration:none; color:blue}')
+         document.writeln('a:hover.link {text-decoration: none; color: red}')
+         document.writeln('//--></style>') }
+
+        function openClose(theID) {
+         if (document.getElementById(theID).style.display == "block") {
+             document.getElementById(theID).style.display = "none";}
+             //document.getElementById("tick_"+theID).innerHTML = "+"; }
+         else {
+             document.getElementById(theID).style.display = "block";}}
+            // document.getElementById("tick_"+theID).innerHTML = "-"; } }
+
+
+         </script>  
+<!-- new -->
+    <body class="w3-content" style="max-width:100%;background-color: #fefbd8;" > 
+
+    <!-- Sidebar/menu -->
+    <nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:200px" id="mySidebar">
+
+     <div class="w3-container w3-display-container w3-padding-16 " >  
+
+      <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+        <!-- <h6 ><b>Index</b></h6> -->
+      </div> 
+   <!-- side Menu -->    
+    <!-- #include file="sidemenu.txt" -->
+    <!-- end side menu --> 
+
+
+    </nav>
+
+    <!-- Top menu on small screens -->
+    <header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge"> <!-- w3 black is the culprit -->
+      <div class="w3-bar-item w3-padding-24 w3-wide">Divine Traveller</div>
+      <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+    </header>
+
+    <!-- Overlay effect when opening sidebar on small screens -->
+    <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+    <!-- !PAGE CONTENT! -->
+    <div class="w3-main" style="margin-left:250px"><span style="padding-left: 20px; font: italic bold 20px/30px Georgia, serif; " >Divine Traveller</span>
+
+      <!-- Push down content on small screens -->
+      <div class="w3-hide-large" style="margin-top:83px"></div>
+
+      <!-- Top header -->
+      <header class="w3-container w3-large">
+          
+        <div class="topnav">
+      <a  class="active" href="index.html">Home</a>  
+      <a href="#contact">Contact</a>
+      <a href="author.html">About</a>
+      <a href="ack.html">Ack</a>
+
+    </div>
+          <div>
+       <script>
+      (function() {
+        var cx = '011269242123454548007:bu3lgpzlvhw';
+        var gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+      })();
+    </script>
+    <gcse:search></gcse:search>
+     </div>     
+
+
+      </header>
+
+
+      <!-- Product grid -->
+
+
+          <div class="w3-container">
+            <img src="img/bannerwith%20title.JPG" style="width:100%">
+
+          </div>
+
+        <div >
+         <h2> Divine Temples of India </h2>
+<!-- AddToAny BEGIN -->
+<div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+<a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+<a class="a2a_button_facebook"></a>
+<a class="a2a_button_twitter"></a>
+<a class="a2a_button_google_plus"></a>
+<a class="a2a_button_whatsapp"></a>
+</div>
+<script async src="https://static.addtoany.com/menu/page.js"></script>
+<!-- AddToAny END -->
+           <p> &nbsp; &nbsp;&nbsp; The present days' lifestyle has become very complex. People, especially the youngsters have no time even to think of God.
+        The temples in India are places that can spur  one's divine thoughts. The sanctity and divinity of these temples soothes and revitalizes his worn-out nerves with the spiritual aura.
+        There are a large number of temples in India, which have attained great following and admiration for their divinity and architectural styles.
+
+        South India is one such place that have so many temples that are not only divine but also symbols of splendor and intricate architecture.<br/>&nbsp; This website takes the readers on a journey through most of the prominent temples of South India and a few very popular must visit temples of rest of the India covering the history,the legend and devotional aspects of these temples. I have tried to provide a detailed information which will help visitors plan their mode of travel, accommodation, temple timings, contact numbers, places around the temple etc that might be useful to the visitors. I also need to mention that these are based on my own experience and readers are cautioned to use their discretion before deciding on their travel to these places.
+         <br /></p>
+
+
+            <div><strong>Disclaimer:</strong>The article about the temple, temple timings and the contact details were based on my own experience. These might have changed over the course of time. The travellers are requested to use use their discretion in such cases.</div>
+        <p>&nbsp;</p>
+
+        </div>
+
+
+
+
+      <!-- Subscribe section -->
+
+
+      <!-- Footer -->
+      <footer class="w3-padding-64 w3-light-grey w3-small w3-center" id="footer">
+
+          <div class="google-maps">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30713422.91222391!2d64.35597324985933!3d20.000239370804277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1520779899514" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+
+    <table width="100%" border="1" align="center">
+            <tr>
+              <td align="center" valign="middle"><script type="text/javascript"><!--
+        google_ad_client = "ca-pub-2331677159054721";
+        /* divine traveller footer */
+        google_ad_slot = "2505517236";
+        google_ad_width = 728;
+        google_ad_height = 90;
+        //-->
+        </script>
+        <script type="text/javascript"
+        src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+        </script></td>
+            </tr>
+          </table>
+           </footer>
+      <div >Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="JamsTechs" target="_blank" class="w3-hover-opacity">JamsTechs</a>&nbsp;2018</div>
+
+      <!-- End page content -->
+    </div>
+
+
+
+
+    <script>
+    // Accordion 
+    function myAccFunc() {
+        var x = document.getElementById("demoAcc");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+        }
+    }
+
+    // Click on the "Jeans" link on page load to open the accordion for demo purposes
+    document.getElementById("myBtn").click();
+
+
+    // Script to open and close sidebar
+    function w3_open() {
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("myOverlay").style.display = "block";
+    }
+
+    function w3_close() {
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("myOverlay").style.display = "none";
+    }
+
+
+    /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+
+    for (i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+        } else {
+          dropdownContent.style.display = "block";
+        }
+      });
+    }
+
+    </script>
+    </body>
+    </html>
